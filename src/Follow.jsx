2ecf59e0e9eae67113d8c323f51b1899d1c81fe7
KareemHossny207 <kareemhossny18@icloud.com>
2025-06-03 03:41:03 +0300

    update App.css

--- src/Follow.jsx
+++ src/Follow.jsx
@@ -1,44 +1,84 @@
 import React from 'react'
-import { MdStar } from "react-icons/md";
-import App from './App';
-import './App.css'
-
-
+import { MdStar } from 'react-icons/md'
 
 const Follow = () => {
   return (
     <div className='follow' id='Follow'>
+      <div className="follow-header">
         <h1>Follow Us</h1>
-        <div className='followSec'>
-          <div className='followSec1'>
-            <img src="\images\img25.jpg"/>
-            <img src="\images\img26.jpg"/>
-            <img src="\images\img27.jpg"/>
+        <p className="follow-subtitle">Join our community of flower enthusiasts</p>
+      </div>
 
+      <div className='followSec'>
+        <div className='followSec1'>
+          <div className="image-container">
+            <img src="/images/img25.jpg" alt="Floral Design"/>
+            <div className="image-overlay">
+              <span className="hover-icon">♥</span>
+            </div>
           </div>
-
-         <div className='followSec1'>
-            <img src="\images\img28.jpg"/>
-             <img src="\images\img29.jpg"/>
-            <img src="\images\img30.jpg"/>
-         </div>
+          <div className="image-container">
+            <img src="/images/img26.jpg" alt="Floral Arrangement"/>
+            <div className="image-overlay">
+              <span className="hover-icon">♥</span>
+            </div>
+          </div>
         </div>
 
-        <div className='followText'>
-            <p>Our Customers Say</p>
-            <h4>Excellent</h4>
-            <span>
-                <MdStar className='star' />
-                <MdStar className='star' />
-                <MdStar className='star' />
-                <MdStar className='star' />
-                <MdStar className='star' />
-            </span>
+        <div className='followSec1'>
+          <div className="image-container">
+            <img src="/images/img28.jpg" alt="Floral Design"/>
+            <div className="image-overlay">
+              <span className="hover-icon">♥</span>
+            </div>
+          </div>
+          <div className="image-container">
+            <img src="/images/img29.jpg" alt="Floral Arrangement"/>
+            <div className="image-overlay">
+              <span className="hover-icon">♥</span>
+            </div>
+          </div>
+        </div>
+
+        <div className='followSec1'>
+          <div className="image-container">
+            <img src="/images/img30.jpg" alt="Flower Display"/>
+            <div className="image-overlay">
+              <span className="hover-icon">♥</span>
+            </div>
+          </div>
+          <div className="image-container">
+            <img src="/images/img27.jpg" alt="Flower Display"/>
+            <div className="image-overlay">
+              <span className="hover-icon">♥</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className='followText'>
+        <div className="review-header">
+          <p>Our Customers Say</p>
+          <h4>Excellent</h4>
+        </div>
+        
+        <div className="rating-stars">
+          {[...Array(5)].map((_, index) => (
+            <MdStar key={index} className='star' />
+          ))}
+        </div>
+        
+        <div className="rating-info">
+          <div className="rating-number">
             <h4>4.8</h4>
             <p>out of 5 based on</p>
-            <h4>7.980 Reviews</h4>
-            <img src="\images\img32.png"/>
+            <h4>7,980 Reviews</h4>
+          </div>
+          <div className="trustpilot-logo">
+            <img src="/images/img32.png" alt="Trustpilot"/>
+          </div>
         </div>
+      </div>
     </div>
   )
 }
--- src/School.jsx
+++ src/School.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import { Link , Router ,  Routes , Route ,BrowserRouter ,NavLink} from 'react-router'
+import { Link } from 'react-router-dom'
 
 const School = () => {
   return (
@@ -8,19 +8,39 @@ const School = () => {
     <div className='schoolContainer'>
      <div className='schoolTitle'>
     <div className='leftSchool'>
-    <img src="\images\img20.webp"/>
+    <div className="image-overlay"></div>
+    <img src="/images/img20.webp" alt="Flower School"/>
+    <div className="school-badge">
+      <span>Est.</span>
+      <span>2025</span>
+    </div>
     </div>
         </div>   
      <div className='schoolContent'>
    <div className="rightSchool">
     <div className='schoolText'>
-        <h2>its Flower</h2>
+        <h2>it's Flower</h2>
         <h2>School</h2>
     </div>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda quidem quisquam debitis impedit sunt
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde doloremque harum maxime, est provident 
-        </p>
-            <Link className='find' to="/Flowers"><h4>Find Out More</h4></Link>
+    <div className="school-features">
+      <div className="feature">
+        <span className="feature-number">01</span>
+        <h3>Expert Instructors</h3>
+        <p>Learn from experienced florists and designers</p>
+      </div>
+      <div className="feature">
+        <span className="feature-number">02</span>
+        <h3>Hands-on Experience</h3>
+        <p>Create beautiful arrangements in our workshops</p>
+      </div>
+    </div>
+    <p className="school-description">
+      Join our flower school and discover the art of floral design. From basic arrangements to advanced techniques, 
+      our courses will help you develop your creative skills and professional expertise.
+    </p>
+    <Link className='find' to="/Flowers">
+      <h4>Find Out More</h4>
+    </Link>
 
         </div>
      </div>
